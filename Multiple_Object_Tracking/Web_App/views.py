5be68c6817e0d1533435934e9ec634583888591c
Daniel Tomaszuk <dtomaszuk01@gmail.com>
2017-07-30 16:56:27 +0200

    Add Otsu binarization function

--- Multiple_Object_Tracking/Web_App/views.py
+++ Multiple_Object_Tracking/Web_App/views.py
@@ -1,26 +1,57 @@
 from django.shortcuts import render
-from numpy import *
+import numpy as np
 from matplotlib import pyplot as plt
 import cv2
 
-# Create your views here.
-plt.close('all')
+
+def otsu_binary(img):
+
+    plt.close('all')
+    blur = cv2.GaussianBlur(img, (5, 5), 0)
+    # find normalized_histogram, and its cumulative distribution function
+    hist = cv2.calcHist([blur], [0], None, [256], [0, 256])
+    hist_norm = hist.ravel() / hist.max()
+    Q = hist_norm.cumsum()
+    bins = np.arange(256)
+    fn_min = np.inf
+    thresh = -1
+
+    for i in range(1, 255):
+        p1, p2 = np.hsplit(hist_norm, [i])  # probabilities
+        q1 = Q[i]
+        q2 = Q[255] - q1  # cum sum of classes
+        b1, b2 = np.hsplit(bins, [i])  # weights
+        # finding means and variances
+        m1 = np.sum(p1 * b1) / q1
+        m2 = np.sum(p2 * b2) / q2
+        v1, v2 = np.sum(((b1 - m1) ** 2) * p1) / q1, np.sum(
+            ((b2 - m2) ** 2) * p2) / q2
+        # calculates the minimization function
+        fn = v1 * q1 + v2 * q2
+        if fn < fn_min:
+            fn_min = fn
+            thresh = i
+    # find otsu's threshold value with OpenCV function
+    ret, otsu = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+    print("{} {}".format(thresh, ret))
+
+    ret, img_thresh1 = cv2.threshold(img, thresh, 255, cv2.THRESH_BINARY)
+    return img_thresh1
+
 
 img = cv2.imread('static/files/hardware.jpg', 0)
+img_thresh1 = otsu_binary(img)
+titles = ['Original Image', 'BINARY']
 
-
-ret, thresh1 = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)
-ret, thresh2 = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY_INV)
-ret, thresh3 = cv2.threshold(img, 127, 255, cv2.THRESH_TRUNC)
-ret, thresh4 = cv2.threshold(img, 127, 255, cv2.THRESH_TOZERO)
-ret, thresh5 = cv2.threshold(img, 127, 255, cv2.THRESH_TOZERO_INV)
-
-
-titles = ['Original Image', 'BINARY', 'BINARY_INV', 'TRUNC', 'TOZERO',
-          'TOZERO_INV']
-images = [img, thresh1, thresh2, thresh3, thresh4, thresh5]
-for i in range(6):
-    plt.subplot(2, 3, i + 1), plt.imshow(images[i], 'gray')
+images = [img, img_thresh1]
+for i in range(2):
+    plt.subplot(1, 2, i + 1), plt.imshow(images[i], 'gray')
     plt.title(titles[i])
     plt.xticks([]), plt.yticks([])
 plt.show()
+
+
+
+
+
+
