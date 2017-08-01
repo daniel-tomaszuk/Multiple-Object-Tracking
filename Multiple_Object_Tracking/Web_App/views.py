301efa3caaf954bf59cf4f43572a5c2b5f2e2f15
Daniel Tomaszuk <dtomaszuk01@gmail.com>
2017-08-01 11:15:47 +0200

    Add function for selecting video fragment

--- Multiple_Object_Tracking/Web_App/views.py
+++ Multiple_Object_Tracking/Web_App/views.py
@@ -4,6 +4,8 @@ from matplotlib import pyplot as plt
 import cv2
 import sys
 import time
+import os
+
 
 def otsu_binary(img):
     """
@@ -51,6 +53,38 @@ def otsu_binary(img):
     ret, img_thresh1 = cv2.threshold(img, thresh, 255, cv2.THRESH_BINARY)
     return img_thresh1
 
+
+def select_frames(video, frame_start, frame_stop):
+    """
+    Function that return selected frames from video.
+    :param video: string, video whom frames are selected,
+    :param frame_start: integer, frame from selection should be started
+    :param frame_stop: integer, ending frame of selected section
+    :return: video fragment <start_frame, stop_frame>
+    """
+    cap = cv2.VideoCapture(video)
+    # font = cv2.FONT_HERSHEY_SIMPLEX
+    video_fragment = []
+    cap.set(1, frame_start)
+
+    while cap.isOpened():
+        ret, frame = cap.read()
+        video_fragment.append(frame)
+        if cv2.waitKey(15) & 0xFF == ord('q') or not ret \
+                or (cap.get(1)) == frame_stop + 1:
+            break
+    #     # img, text, (x,y), font, size, color, thickens
+    #     cv2.putText(frame, str(round(cap.get(0)/1000, 2)) + 's',
+    #                 (10, 15), font, 0.5, (255, 255, 255), 1)
+    #     cv2.putText(frame, 'f.nr:' + str(cap.get(1)),
+    #                 (100, 15), font, 0.5, (255, 255, 255), 1)
+    #     cv2.imshow('frame', frame)
+
+    cap.release()
+    cv2.destroyAllWindows()
+    return video_fragment
+
+
 # img = cv2.imread('static/files/hardware.jpg')
 # img_thresh1 = otsu_binary(img)
 # titles = ['Original Image', 'BINARY']
@@ -67,19 +101,21 @@ def otsu_binary(img):
 # list of all VideoCapture methods and attributes
 # [print(method) for method in dir(cap) if callable(getattr(cap, method))]
 
-cap = cv2.VideoCapture('static/files/small.avi')
-ret, frame = cap.read()
+font = cv2.FONT_HERSHEY_SIMPLEX
+vid_fragment = select_frames('static/files/CIMG4027.MOV', 1000, 1100)
+i = 0
+for frame in vid_fragment:
+        # img, text, (x,y), font, size, color, thickens
+    cv2.putText(frame, 'f.nr:' + str(1000 + i + 1),
+                    (100, 15), font, 0.5, (255, 255, 255), 1)
+    if cv2.waitKey(15) & 0xFF == ord('q'):
+        break
 
-print(ret)
-print(cap.isOpened())
+    cv2.imshow('frame', frame)
+    i += 1
+input('Press enter in the console to exit..')
+cv2.destroyAllWindows()
 
 
-while(cap.isOpened()):
-    ret, frame = cap.read()
-    print(cap.read())
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    plt.imshow(gray)
-    plt.show()
 
-cap.release()
 
